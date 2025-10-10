ab5b1f7e74383e4cecbe077c8660bb4f030976f3
Jun Aishima <jaishima@bnl.gov>
2025-10-10 11:59:02 -0400

    remove task that cannot be run now
     * use Prefect logging to replace bare print()

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,12 +1,13 @@
-from prefect import task, flow
+from prefect import task, flow, get_run_logger
 from data_validation import data_validation
 
 @task
 def log_completion():
-    print("Complete")
+    logger = get_run_logger()
+    logger.info("Complete")
 
 @flow(log_prints=True)
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    data_validation(uid)
+    #data_validation(uid)
     log_completion()
